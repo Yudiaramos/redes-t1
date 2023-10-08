0f48de9eb3a4aab6b8512138874bd3ec9a4cce1b
Yudi Asano Ramos <71808184+Yudiaramos@users.noreply.github.com>
2023-10-08 16:50:49 -0300

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,53 @@
+import socket
+import threading
+
+# Configurações do cliente
+HOST = '127.0.0.1'  # Endereço IP do servidor
+PORT = 12355      # Porta do servidor
+
+# Função para ler mensagens do servidor
+def receive_messages(client_socket):
+    while True:
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                # Se não houver dados, o servidor desconectou
+                print('Conexão com o servidor perdida.')
+                client_socket.close()
+                break
+            print(data.decode('utf-8'))
+        except Exception as e:
+            print(f'Erro na comunicação com o servidor: {e}')
+            client_socket.close()
+            break
+
+# Configuração do cliente
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+print(f'Conectado ao servidor em {HOST}:{PORT}')
+
+# Inicia uma thread para receber mensagens do servidor
+receive_thread = threading.Thread(target=receive_messages, args=(client,))
+receive_thread.start()
+
+# Loop principal para enviar mensagens
+while True:
+    try:
+        mensagem = input()
+        if mensagem.startswith('/criar'):
+            client.send(mensagem.encode('utf-8'))
+        elif mensagem.startswith('/listar'):
+            client.send(mensagem.encode('utf-8'))
+        elif mensagem.startswith('/conectar'):
+            client.send(mensagem.encode('utf-8'))
+        else:
+            client.send(mensagem.encode('utf-8'))
+    except KeyboardInterrupt:
+        print('Cliente encerrado.')
+        client.close()
+        break
+    except Exception as e:
+        print(f'Erro ao enviar mensagem: {e}')
+        client.close()
+        break
+
